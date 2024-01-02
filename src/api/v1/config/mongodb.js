fa4f15e42ea8e8e0798ce0027d8a3410387580a1
Nikit_dwivedi <nikitdwivedi1997@gmail.com>
2024-01-02 15:25:41 +0530

    change

--- src/api/v1/config/mongodb.js
+++ src/api/v1/config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 db = mongoose.connect(
-  "mongodb+srv://nikit:nikit@cluster0.5zi8b.mongodb.net/wallpaperX?retryWrites=true&w=majority",
+  "",
   (err) => {
     console.log("Database connected");
     if (err) {
